9f1def7da1be134e1475655dbcb8c1c9a2460a13
m031416j@student.staffs.ac.uk <brendan.muldoon@bt.com>
2023-04-14 11:05:20 +0100

    Add About page text

--- src/components/AppAbout.vue
+++ src/components/AppAbout.vue
@@ -2,38 +2,26 @@
   <div class="container">
     <h2>About</h2>
     <p>
-      Once upon a time, in a land far away, there lived a wise old wizard named
-      Merlin. He lived in a castle on the top of a mountain, where he spent his
-      days studying magic and observing the world below. One day, a young prince
-      named Arthur arrived at the castle seeking Merlin's help. Arthur had been
-      told that he was destined to be a great king, but he didn't believe it. He
-      wanted Merlin to show him a sign that he was indeed meant to rule. Merlin
-      took Arthur on a journey through the land, showing him the beauty and the
-      hardships that the people faced. They met farmers and fishermen, knights
-      and bards, all of whom had their own stories to tell. As they traveled,
-      Arthur began to see that being a king meant more than just having power
-      and wealth. It meant having a responsibility to the people, to make their
-      lives better. Finally, they came to a small village, where the people were
-      struggling to survive. There was no food or water, and the villagers were
-      in despair. Merlin told Arthur that this was his chance to prove himself
-      as a leader. With Merlin's guidance, Arthur organized the people and
-      worked alongside them to build a well and plant crops. They worked day and
-      night, and finally, the village had food and water once again. Arthur had
-      proven that he had the heart of a king, and he returned to his kingdom
-      w
-     
+      The application aims to simplify and streamline the process of tracking
+      APIs and identifying errors. It provides a uniformed way of monitoring
+      APIs and reduces the time and effort required to investigate issues. The
+      application is designed as a prototype and currently only supports Spring Boot
+      applications. With its user-friendly interface and comprehensive features,
+      the application is a valuable tool for developers and IT professionals who
+      want to optimize their API management and troubleshooting processes. By
+      leveraging the power of this application, developers can minimize errors,
+      improve efficiency, and ultimately, deliver better products and services.
     </p>
   </div>
 </template>
 
 <script>
 export default {
-  name: "TestVue",
+  name: "AppAbout",
 };
 </script>
 
 <style scoped>
-
 h2 {
   font-size: 2.5rem;
   padding-top: 50px;
@@ -42,13 +30,12 @@ h2 {
 }
 
 p {
-  font-size: 1.25rem;
+  font-size: 1.75rem;
   padding-top: 4rem;
   padding-bottom: 5rem;
   padding-right: 10rem;
   padding-left: 10rem;
   text-align: left;
   margin-bottom: -5px;
-
 }
 </style>
